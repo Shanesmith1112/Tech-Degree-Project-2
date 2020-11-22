b07559f30912ddae098a0909d40d5f77ee7310bb
Shanesmith1112 <72278992+Shanesmith1112@users.noreply.github.com>
2020-11-22 00:48:37 -0500

    updated While True statements

--- main_function_project_2.py
+++ main_function_project_2.py
@@ -1,5 +1,6 @@
 import sys
-#import constants 
+
+# import constants
 
 print("******** BASKETBALL TEAM STATS TOOL ********")
 
@@ -8,34 +9,36 @@ Panthers = []
 Warriors = []
 
 Players = []
-#players[] may not be needed in the end 
 
 
-#def clean_data():
-    #store all data in proper data forms ie numbers as ints and variables as strings 
-    # .append new data to players[] ??
+# players[] may not be needed in the end
 
 
-#def balance_teams():
-    #balance teams and store them in proper lists with appropriate information
+# def clean_data():
+# store all data in proper data forms ie numbers as ints and variables as strings
+# .append new data to players[] ??
+
+
+# def balance_teams():
+# balance teams and store them in proper lists with appropriate information
 
 
 def main():
     print('\n1.) Bandits\n2.) Panthers\n3.) Warriors\n')
-    while 1 == 1:
+    while True:
         try:
             team_choice = int(input('Please enter an option > '))
             if team_choice < 1 or team_choice > 3:
                 print('\nERROR!\nThat is not a valid option\n')
-           #elif team_choice == 1:
-                # display Bandits stats
-                # press enter to continue
-           #elif team_choice == 2:
-                # display Panthers stats
-                # press enter to continue
-           #elif team_choice == 3:
-                # display Warriors stats
-                # press enter to continue
+            # elif team_choice == 1:
+            # display Bandits stats
+            # press enter to continue
+            # elif team_choice == 2:
+            # display Panthers stats
+            # press enter to continue
+            # elif team_choice == 3:
+            # display Warriors stats
+            # press enter to continue
         except ValueError:
             print('\nERROR!\nThat is not a valid option, please choose and option from the list\n')
             pass
@@ -44,23 +47,22 @@ def main():
 def menu():
     print("\n\n --- MENU --- \n\n")
     print('Here are your choices:\n 1.) Display Team Stats\n 2.) Quit\n')
-    while 1 == 1:
+    while True:
         try:
             choice = int(input('Please enter an option > '))
             if choice == 2:
                 print('\nThank you for using Basketball Team Stats Tool')
                 exit()
-            elif choice < 1 or choice >2:
-                print('\nERROR!\nThat is not a valid option.\n') 
+            elif choice < 1 or choice > 2:
+                print('\nERROR!\nThat is not a valid option.\n')
             elif choice == 1:
                 main()
         except ValueError:
             print('\nERROR!\nThat is not a valid option please enter an option from the list\n')
-            continue 
+            continue
 
 
 if __name__ == '__main__':
-    #clean_data()
-    #balance_teams()
+    # clean_data()
+    # balance_teams()
     menu()
-            
