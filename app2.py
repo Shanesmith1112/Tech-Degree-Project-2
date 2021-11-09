fd9fb3726e2639b02f9b04ec1cfc7702cd4dd59f
Shane Smith <72278992+Shanesmith1112@users.noreply.github.com>
2021-11-09 18:18:16 -0500

    finished the team balancing functions

--- app2.py
+++ app2.py
@@ -1,12 +1,17 @@
 import copy
-import sys
 import constants
+import sys
 
 
-player_info = [player for player in constants.PLAYERS.copy()]
-Bandits = []
-Panthers = []
-Warriors = []
+player_info = copy.deepcopy(constants.PLAYERS)
+teams = copy.deepcopy(constants.TEAMS)
+panthers = []
+bandits = []
+warriors = []
+exp_players = []
+noexp_players = []
+
+num_players_team = int(len(constants.PLAYERS) / len(constants.TEAMS))
 
 print("******** BASKETBALL TEAM STATS TOOL ********")
 
@@ -14,18 +19,38 @@ print("******** BASKETBALL TEAM STATS TOOL ********")
 def clean_data():
     for player in player_info:
         # store height as an int
-        player['height'] = int(player['height'][:2])
+        player["height"] = int(player["height"][:2])
         #store experience as a boolean value (true/false)
         if player['experience'] == "YES":
-            player['experience'] = "True"
-        if player['experience'] != "YES":
+            player['experience'] = "TRUE"
+            exp_players.append(player)
+        if player['experience'] == "NO":
             player['experience'] = "FALSE"
+            noexp_players.append(player)
+        # Additionally clean the guardians string so that it becomes a List of strings. Remove the and between the names and storing each guardian in a List together for that player.
+        player['guardians'] = player['guardians'].split(" and ")
+
+# Balance teams
+
+def balance_exp():
+    num_exp_players = int(len(exp_players) / len(constants.TEAMS))
+    for player in exp_players:
+        if len(panthers) < num_exp_players:
+            panthers.append(player)
+        elif len(bandits) < num_exp_players:
+            bandits.append(player)
+        elif len(warriors) < num_exp_players:
+            warriors.append(player)
 
 
-def balance_teams():
-    # balance teams and store them in proper lists with appropriate information
-    for player in player_info:
-    # example num_players_team = len(PLAYERS) / len(TEAMS)
+def balance_noexp():
+    for player in noexp_players:
+        if len(panthers) < num_players_team:
+            panthers.append(player)
+        elif len(bandits) < num_players_team:
+            bandits.append(player)
+        elif len(warriors) < num_players_team:
+            warriors.append(player)
 
 
 def press_enter():
@@ -35,6 +60,7 @@ def press_enter():
     else:
         menu()
 
+
 def main():
     print('\n1.) Bandits\n2.) Panthers\n3.) Warriors\n')
     while True:
@@ -43,15 +69,18 @@ def main():
             if team_choice < 1 or team_choice > 3:
                 print('\nERROR!\nThat is not a valid option\n')
             elif team_choice == 1:
-                print(Bandits)
+                print(bandits)
+                # expand on this to format properly
                 press_enter()
             # press enter to continue
             elif team_choice == 2:
-                print(Panthers)
+                print(panthers)
+                # expand on this to format properly
                 press_enter()
             # press enter to continue
             elif team_choice == 3:
-                print(Warriors)
+                print(warriors)
+                # expand on this to format properly
                 press_enter()
             # press enter to continue
                 # teams name is a string
@@ -83,5 +112,9 @@ def menu():
 
 if __name__ == '__main__':
     clean_data()
-    balance_teams()
+    balance_exp()
+    balance_noexp()
     menu()
+
+
+
